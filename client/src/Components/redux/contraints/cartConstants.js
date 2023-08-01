baecf7bfac8ce32f2faf88c75db9f53a94520b13
Manojmm98 <manojmishra98feb@gmail.com>
2023-08-01 22:35:46 +0530

    Till project finish

--- client/src/Components/redux/contraints/cartConstants.js
+++
@@ -1,4 +0,0 @@
-
-export const ADD_TO_CART = 'addtoCart' 
-export const REMOVE_FROM_CART = 'removeFromCart' 
-export const ADD_TO_CART_ERROR = 'addtoCartError'
--- client/src/Components/redux/contraints/productConstants.js
+++
@@ -1,7 +0,0 @@
-export const GET_PRODUCT_SUCESS = 'getproductSucess'
-export const GET_PRODUCT_FAIL = 'getproductFail'
-
-export const GET_PRODUCT_DETAIL_REQUEST = 'getproductdetailsRequest'
-export const GET_PRODUCT_DETAIL_SUCESS = 'getproductdetailsSucess'
-export const GET_PRODUCT_DETAIL_FAIL = 'getproductdetailsFail'
-export const GET_PRODUCT_DETAIL_RESET = 'getproductdetailsReset'
